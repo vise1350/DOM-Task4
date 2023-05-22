985670b2e6fe830a30009e7eebfe922deb87c230
vise1350 <vise1350@gmail.com>
2023-05-22 11:58:51 +0530

    first commit

---
+++ Task 4/dom.js
@@ -0,0 +1,11 @@
+// var ht=(document.getElementById("header-title"))
+// console.log(ht)
+// ht.textContent="hello"
+// ht.style.borderBottom='solid 3px #000'
+var items=document.getElementsByClassName("list-group-item")
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold"
+
+}
+
+document.querySelectorAll(".list-group-item")[2].style.backgroundColor="green"
